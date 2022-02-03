06b971d29feaae0514be6c03d51efdfc180683b7
zackpyle <zack@zackpyle.com>
2022-02-03 09:42:45 -0600

    Code comments

--- js/hidden-class-toggle.js
+++ js/hidden-class-toggle.js
@@ -1,15 +1,17 @@
 jQuery(document).ready(function () {
+  // Add button to BB Menu
   jQuery(".fl-builder--menu").append(
     '<button class="fl-builder--menu-item hidden-class-btn" data-type="event" data-event="hiddenClassToggle">Toggle Hidden Classes<span class="menu-event event-hiddenClassToggle"></span><span class="fl-builder--menu-item-accessory">⌘=</span></button>'
   );
-
+  // Toggle body class on button click
   jQuery(".hidden-class-btn").on("click", function () {
     jQuery("body").toggleClass("show-hidden-classes");
   });
-
+  // Toggle body class on shortcut
   function hiddenClassToggle() {
     jQuery("body").toggleClass("show-hidden-classes");
   }
+  // Add shortcut to BB
   jQuery(function () {
     FLBuilder.addHook("hiddenClassToggle", hiddenClassToggle);
   });
